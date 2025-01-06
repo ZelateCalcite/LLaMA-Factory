e23d60ad060d9a391965e8411063a4a318d6c717
st-fei <1982861687@qq.com>
2025-01-06 17:52:56 +0800

    update configs

--- api_lora.sh
+++ api_lora.sh
@@ -1,2 +1,2 @@
 API_PORT=8000
-llamafactory-cli api examples/inference/qwen_lora_sft.yaml
+llamafactory-cli api examples/inference/qwen_lora_sft.yaml >> logs/run/run_14b_ar_e10.log 2>&1
--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 
-llamafactory-cli train examples/train_qlora/qwen_lora_sft_bitsandbytes.yaml >> logs/train_14b_de_e50.log 2>&1 &
+llamafactory-cli train examples/train_qlora/qwen_lora_sft_bitsandbytes.yaml >> logs/train/train_14b_ar_e5.log 2>&1
