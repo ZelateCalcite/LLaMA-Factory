ebe7bdcedb23928454f09d4e6c0605b7a30d8288
zzh-10.10.66.38 <1105901242@qq.com>
2025-01-06 11:50:19 +0800

    add corpus update config

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 
-llamafactory-cli train examples/train_qlora/qwen_lora_sft_bitsandbytes.yaml >> logs/train_14b_e50.log
+llamafactory-cli train examples/train_qlora/qwen_lora_sft_bitsandbytes.yaml >> logs/train_14b_de_e50.log 2>&1 &
